c10b785744c1bccf5846f6cf983c4e2dbc375a3f
Garrett Hoffman <son0hoff@gmail.com>
2023-07-28 16:39:03 -0700

    start off with Sign in link

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { FC } from "react";
 
 const page: FC = () => {
-  return <div>page</div>;
+  return (
+    <div className="absolute inset-0">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+        <Link href="/" className={cn(buttonVariants({ variant: "ghost" }))}>
+          Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default page;
